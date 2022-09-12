99eb5d8ccd95093e194d80393da503c833d880c7
Amit Singh <amit.mot.08@gmail.com>
2022-09-13 06:54:51 +1000

    Video player

--- app/src/main/java/com/example/videoplayer/MainActivity.kt
+++ app/src/main/java/com/example/videoplayer/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.videoplayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.videoplayer.ui.Video
 import com.example.videoplayer.ui.theme.VideoPlayerTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +11,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VideoPlayerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+               Video(
+                   lifecycleOwner = this
+               )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    VideoPlayerTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/videoplayer/ui/Video.kt
+++ app/src/main/java/com/example/videoplayer/ui/Video.kt
@@ -1,16 +1,24 @@
 package com.example.videoplayer.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.videoplayer.VideoViewModel
 
 @Composable
-fun Video() {
+fun Video(
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
+) {
     val viewModel: VideoViewModel = viewModel()
 
     VideoPlayer(
+        lifecycleOwner = lifecycleOwner,
         videoState = viewModel.uiState.collectAsState().value,
-        handleEvent = viewModel::handleEvent
+        handleEvent = viewModel::handleEvent,
+        modifier = Modifier.fillMaxSize()
     )
 }
