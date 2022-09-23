fdbf97e5c5f3f9298e091e21fa735cb1ed7b36bf
Amit Singh <amit.mot.08@gmail.com>
2022-09-23 22:06:12 +1000

    Playback control visibility

--- app/src/main/java/com/example/videoplayer/ui/VideoPlayer.kt
+++ app/src/main/java/com/example/videoplayer/ui/VideoPlayer.kt
@@ -1,20 +1,24 @@
 package com.example.videoplayer.ui
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.LifecycleOwner
 import com.example.videoplayer.model.PlayerStatus
 import com.example.videoplayer.model.VideoEvent
 import com.example.videoplayer.model.VideoState
+import com.example.videoplayer.R
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
@@ -46,19 +50,45 @@ fun VideoPlayer(
     Box(
         modifier = Modifier.background(Color.Black)
     ) {
+        var controlVisible by remember {
+            mutableStateOf(true)
+        }
+
+        val controlsClickLabel = if (controlVisible) {
+            stringResource(id = R.string.cd_hide_controls)
+        } else {
+            stringResource(id = R.string.cd_display_controls)
+        }
+
+        val alphaAnimation by animateFloatAsState(
+            targetValue = if(controlVisible) 0.7f else 0f,
+            animationSpec = if(controlVisible) tween(0) else tween(750)
+        )
+
         Playback(
             exoPlayer = exoPlayer,
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable(
+                    onClickLabel = controlsClickLabel
+                ) {
+                    controlVisible = !controlVisible
+                },
             lifecycleOwner = lifecycleOwner,
             state = videoState.playerStatus
         )
 
         Controls(
-            modifier = Modifier.fillMaxWidth()
-                .align(Alignment.BottomCenter),
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .alpha(alphaAnimation),
             playerStatus = videoState.playerStatus,
             togglePlayingState = {
                 handleEvent(VideoEvent.ToggleStatus)
+                if(videoState.playerStatus != PlayerStatus.PLAYING) {
+                    controlVisible = false
+                }
             }
         )
     }
